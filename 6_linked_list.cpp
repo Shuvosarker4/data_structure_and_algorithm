d25124f9672d81eee0c5a8d8547d82191ab94ff6
Shuvosarker4 <shuvo01797451@gmail.com>
2024-12-17 13:48:03 +0600

    liked_list

---
+++ 6_linked_list.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // there are different github repo for doubly and singly linked list
+
+    // url link
+    // https://github.com/Shuvosarker4/Linked_List
+    return 0;
+}
